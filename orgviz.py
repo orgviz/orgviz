a66e9cdec1a67dcec42ec23ce202f2565b7186ac
jamesread <contact@jwread.com>
2020-04-29 17:22:04 +0100

    fix workdir

--- orgviz.py
+++ orgviz.py
@@ -8,7 +8,7 @@ import os
 
 import sys
 
-sys.path.insert(0, "./orgviz")
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "orgviz"))
 
 from orgviz.modelToGraphviz import ModelToGraphVizConverter
 from orgviz.modelParser import parseModel
